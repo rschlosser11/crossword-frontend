028e5bf92bed2054f4ae81d433344db7b5bfedf2
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-09 16:46:43 -0500

    create store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import manageCrosswords from './reducers/ManageCrosswords';
+import thunk from 'redux-thunk';
+
+const store = createStore(manageCrosswords, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
