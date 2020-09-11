68d077a04cfb27f24aceacc8e03b3bfa7e55fa21
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-11 10:29:12 -0500

    remove unneeded mapPropsToState

--- src/components/Crossword.js
+++ src/components/Crossword.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Grid from './Grid';
-import { connect } from 'react-redux';
 import Clues from './Clues';
 
-function Crossword ({ chosenCrswd }) {
+export default function Crossword ({ chosenCrswd }) {
     return (
         <Container>
             <Grid />
@@ -12,11 +11,3 @@ function Crossword ({ chosenCrswd }) {
         </Container>
     )
 }
-
-const mapStateToProps = (state) => {
-    return {
-        chosenCrswd: state.chosenCrswd
-    }
-  }
-
-export default connect(mapStateToProps)(Crossword)
