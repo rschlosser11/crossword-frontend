fa984b82e0d8a8c74d18327281005888164c5626
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-12 12:09:30 -0500

    add numbers to crossword grid and style

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -12,7 +12,7 @@ class Grid extends React.Component {
         return(
             <Container style={style} >
                 {crossword.grid.map((box, i) => {
-                    return <GridBox letter={box} />
+                    return <GridBox letter={box} num={crossword.gridnums[i]} />
                 })}
             </Container>
         )
--- src/components/GridBox.js
+++ src/components/GridBox.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default function GridBox ({letter}) {
+export default function GridBox ({letter, num}) {
     return (
         <div className={`${letter === '.' ? 'box empty' : 'box'}`}>
+            {num === 0 ? '' : <span className='box-label'>{num}</span>}
             {letter === '.' ? '' : <input className='box-input' type='text' maxLength='1' ></input>}
         </div>
     )
