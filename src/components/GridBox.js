2b5865dde99e2ba6c08d44f793ca2c8e708fe380
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-12 11:58:54 -0500

    add input to boxes and style

--- src/components/GridBox.js
+++ src/components/GridBox.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function GridBox ({letter}) {
     return (
         <div className={`${letter === '.' ? 'box empty' : 'box'}`}>
+            {letter === '.' ? '' : <input className='box-input' type='text' maxLength='1' ></input>}
         </div>
     )
 }
