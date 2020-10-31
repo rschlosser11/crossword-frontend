10bf358b0e4e65d9e4b7a56b6c22256774af006a
rschlosser11 <rosalinschlosser@gmail.com>
2020-10-31 12:18:26 -0500

    connect state to props

--- src/components/CrosswordMenu.js
+++ src/components/CrosswordMenu.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from '../images/crossword-logo.png'
+import logo from '../images/crossword-logo.png';
+import { connect } from 'react-redux';
 
-function CrosswordMenu () {
-    return (
-        <Navbar bg='light' expand='lg'>
-            <Navbar.Brand><Link to='/'><img style={{width: '250px'}} src={logo} alt='Crossword Puzzles' /></Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav className='justify-content-end' style={{width: '100%'}}>
-                    <NavDropdown title='Check' id='basic-nav-dropdown'>
-                        <NavDropdown.Item>Check Letter</NavDropdown.Item>
-                        <NavDropdown.Item>Check Clue</NavDropdown.Item>
-                        <NavDropdown.Item>Check Entire Grid</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title='Reveal' id='basic-nav-dropdown'>
-                        <NavDropdown.Item>Reveal Letter</NavDropdown.Item>
-                        <NavDropdown.Item>Reveal Clue</NavDropdown.Item>
-                        <NavDropdown.Item>Reveal Entire Grid</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+class CrosswordMenu extends React.Component {
+    render() {
+        return (
+            <Navbar bg='light' expand='lg'>
+                <Navbar.Brand><Link to='/'><img style={{width: '250px'}} src={logo} alt='Crossword Puzzles' /></Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='justify-content-end' style={{width: '100%'}}>
+                        <NavDropdown title='Check' id='basic-nav-dropdown'>
+                            <NavDropdown.Item>Check Letter</NavDropdown.Item>
+                            <NavDropdown.Item>Check Clue</NavDropdown.Item>
+                            <NavDropdown.Item>Check Entire Grid</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title='Reveal' id='basic-nav-dropdown'>
+                            <NavDropdown.Item>Reveal Letter</NavDropdown.Item>
+                            <NavDropdown.Item>Reveal Clue</NavDropdown.Item>
+                            <NavDropdown.Item>Reveal Entire Grid</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
 
-export default CrosswordMenu;
+const mapStateToProps = (state) => {
+    return {
+        chosenCrswd: state.chosenCrswd,
+        activeClue: state.activeClue,
+        activeBoxes: state.activeBoxes,
+    }
+}
+
+export default connect(mapStateToProps)(CrosswordMenu);
